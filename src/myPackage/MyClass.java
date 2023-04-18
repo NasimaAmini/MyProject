fa403b92d6504513be51acac42dd8a17e46fea93
nasim <nasim@Nasima>
2023-04-18 02:37:54 -0400

    Added Map

---
+++ src/myPackage/MyClass.java
@@ -0,0 +1,21 @@
+package myPackage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyClass {
+	
+	public static void main(String[] args) {
+		HashMap<String, Integer> employees = new HashMap<>();
+		employees.put("Adam", 5);
+		employees.put("Zaki", 1);
+		employees.put("Karen", 4);
+		employees.put("Luis", 3);
+		employees.put("Juan", 2);
+		System.out.println(employees);
+		
+		ArrayList<Map.Entry<String, Integer>> list = new ArrayList<>();
+
+}
+}
